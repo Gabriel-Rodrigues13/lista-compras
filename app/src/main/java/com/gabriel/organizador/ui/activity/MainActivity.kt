06ee4aa5326da98ac8d302e17a09f789f8e388d5
Gabriel Rodrigues <gabriel@MacBook-Pro-de-Gabriel.local>
2023-07-04 12:15:15 -0300

    Criando layout do produto

---
+++ app/src/main/java/com/gabriel/organizador/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gabriel.organizador.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gabriel.organizador.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
