52da041f1c5a6c25d7fd4a1190347b695de88165
Gabriel Rodrigues <gabriel@MacBook-Pro-de-Gabriel.local>
2023-07-04 14:10:05 -0300

    Implementando recyclerView

--- app/src/main/java/com/gabriel/organizador/ui/activity/MainActivity.kt
+++ app/src/main/java/com/gabriel/organizador/ui/activity/MainActivity.kt
@@ -2,11 +2,30 @@ package com.gabriel.organizador.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.gabriel.organizador.R
+import com.gabriel.organizador.model.Produto
+import com.gabriel.organizador.ui.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
+            Produto(nome = "teste",
+                descricao = "teste desc",
+                valor = BigDecimal("19.99")
+            ),
+            Produto(nome = "teste 1",
+                descricao = "teste desc 1",
+                valor = BigDecimal("29.99")
+            ),
+            Produto(nome = "teste 2",
+                descricao = "teste desc 2",
+                valor = BigDecimal("39.99")
+            ),
+        ))
     }
 }
