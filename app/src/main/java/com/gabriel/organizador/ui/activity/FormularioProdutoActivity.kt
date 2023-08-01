8cf1657213a43bfbe78cbd2c545d13d841f09469
Gabriel Rodrigues <rodrigues.gabriel1307@gmail.com>
2023-08-01 12:32:05 -0300

    Implementando o layout do dialog

--- app/src/main/java/com/gabriel/organizador/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/gabriel/organizador/ui/activity/FormularioProdutoActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import com.gabriel.organizador.R
 import com.gabriel.organizador.dao.ProdutosDao
 import com.gabriel.organizador.databinding.ActivityFormularioProdutoBinding
@@ -12,7 +13,7 @@ import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity() {
 
-    private val binding by lazy{
+    private val binding by lazy {
         ActivityFormularioProdutoBinding.inflate(layoutInflater)
     }
 
@@ -20,6 +21,13 @@ class FormularioProdutoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         configuraBotaoSalvar()
         setContentView(binding.root)
+        binding.imagemFormulario.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setView(R.layout.formulario_imagem)
+                .setPositiveButton("Confirma") { _, _ -> }
+                .setNegativeButton("Cancela") { _, _ -> }
+                .show()
+        }
     }
 
     private fun configuraBotaoSalvar() {
@@ -31,7 +39,7 @@ class FormularioProdutoActivity : AppCompatActivity() {
             finish()
         }
     }
-    
+
     private fun criaProduto(): Produto {
         val campoNome = binding.nome
         val nome = campoNome.text.toString()
--- app/src/main/java/com/gabriel/organizador/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/com/gabriel/organizador/ui/activity/ListaProdutosActivity.kt
@@ -3,26 +3,28 @@ package com.gabriel.organizador.ui.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.app.AlertDialog
 import com.gabriel.organizador.R
 import com.gabriel.organizador.dao.ProdutosDao
 import com.gabriel.organizador.databinding.ActivityListaProdutosActivityBinding
 import com.gabriel.organizador.ui.adapter.ListaProdutosAdapter
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class ListaProdutosActivity : AppCompatActivity() {
-    private val binding by lazy{
+    private val binding by lazy {
         ActivityListaProdutosActivityBinding.inflate(layoutInflater)
     }
     private val dao = ProdutosDao()
     private val adapter = ListaProdutosAdapter(
         context = this, produtos = dao.buscaTodos()
     )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         configuraRecyclerView()
         configuraFab()
+
     }
 
     override fun onResume() {
